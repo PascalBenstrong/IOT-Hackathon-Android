ce3743f7241a6a42fed79f9595abdcc03abe538d
Pascal Nsunba <nsumbapascal@gmail.com>
2020-11-20 03:44:43 +0200

    merged with main

--- app/src/main/java/com/example/iotfactory/MainActivity.java
+++ app/src/main/java/com/example/iotfactory/MainActivity.java
@@ -5,37 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.influxdb.query.FluxTable;
 
 import java.util.List;
 
+import me.itangqi.waveloadingview.WaveLoadingView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private WaveLoadingView waveLoadingView;
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String BrightnessQuery = "from(bucket: \"3261957's Bucket\")\n" +
+        waveLoadingView = findViewById(R.id.wave_progress);
+        lineChart = findViewById(R.id.line_graph);
+
+        loadEnvironmentData();
+    }
+
+    private void loadEnvironmentData(){
+
+        String environment = "from(bucket: \"3261957's Bucket\")\n" +
                 "  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n" +
-                "  |> filter(fn: (r) => r[\"_measurement\"] == \"Brightness Sensor\")\n" +
-                "  |> filter(fn: (r) => r[\"_field\"] == \"br\")\n" +
-                "  |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)\n" +
-                "  |> yield(name: \"mean\")";
-        String TempAndAirQuery = "from(bucket: \"3261957's Bucket\")\n" +
-                "  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n" +
-                "  |> filter(fn: (r) => r[\"_measurement\"] == \"Brightness Sensor\")\n" +
-                "  |> filter(fn: (r) => r[\"_field\"] == \"br\")\n" +
+                "  |> filter(fn: (r) => r[\"_measurement\"] == \"Environment Sensor\")\n" +
+                "  |> filter(fn: (r) => r[\"_field\"] == \"t\" or r[\"_field\"] == \"iaq\")\n" +
                 "  |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)\n" +
                 "  |> yield(name: \"mean\")";
 
-        InfluxDbClientHelper.queryAsync(BrightnessQuery, new InfluxDbClientHelper.OnQueryResponseListener() {
+
+        InfluxDbClientHelper.queryAsync(environment, new InfluxDbClientHelper.OnQueryResponseListener() {
             @Override
             public void OnResponse(List<FluxTable> tables) {
                 Toast.makeText(getBaseContext(), String.format("tables %s", tables.size()), Toast.LENGTH_LONG).show();
             }
         });
+
     }
+
 }
 
 
